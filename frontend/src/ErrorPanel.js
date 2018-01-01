bdda7002612d952e98eee30e3b85e56dfd5183ad
Aleksander Grzybowski <kelog@orange.pl>
2018-01-01 16:08:27 +0100

    Add refresh link to error message component

--- frontend/src/ErrorPanel.js
+++ frontend/src/ErrorPanel.js
@@ -3,10 +3,21 @@ import { Alert } from 'react-bootstrap';
 
 export default function ErrorPanel({message}) {
     // noinspection JSConstructorReturnsPrimitive
-    
+
     return message === '' ? null : (
       <Alert bsStyle="danger">
           <h4>{message}</h4>
+          {/* I'm not a frontend dev */}
+          <h4>
+              Try&nbsp;
+              <span
+                style={{cursor: 'pointer', fontWeight: 'bold', textDecoration: 'underline'}}
+                onClick={() => window.location.reload()}
+              >
+                  refreshing 
+              </span>
+              &nbsp;the page.
+          </h4>
       </Alert>
     );
 }
