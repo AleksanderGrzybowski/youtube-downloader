90480cf7320ea4c1e00155cf84caee697ef10960
Aleksander Grzybowski <kelog@orange.pl>
2018-01-06 11:06:48 +0100

    Add custom async executor per documentation

--- src/main/java/pl/kelog/ytdownloader/YtdownloaderApplication.java
+++ src/main/java/pl/kelog/ytdownloader/YtdownloaderApplication.java
@@ -2,7 +2,11 @@ package pl.kelog.ytdownloader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
 @EnableAsync
@@ -10,6 +14,17 @@ public class YtdownloaderApplication {
     
     public static void main(String[] args) {
         SpringApplication.run(YtdownloaderApplication.class, args);
+    }
+    
+    @Bean
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("spring-async-ex-");
+        executor.initialize();
+        return executor;
     }
 }
 
